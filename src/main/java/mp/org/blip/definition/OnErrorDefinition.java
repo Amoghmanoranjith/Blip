b7ec7a778a419fbacb81851d2735ec78dcf0d1e0
amoghmanoranjith <amoghmanoranjith@gmail.com>
2026-06-03 08:48:40 +0530

    added on error definition

---
+++ src/main/java/mp/org/blip/definition/OnErrorDefinition.java
@@ -0,0 +1,22 @@
+package mp.org.blip.definition;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+//generic.on_error
+public class OnErrorDefinition {
+    // if on_error is given then these
+
+    @NotBlank(message = "{generic.on_error.action.required}")
+    private String action;
+    @NotNull(message = "{generic.on_error.config.required}")
+    private Object config;
+}
