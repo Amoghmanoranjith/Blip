b531765a5edc161034294e86f7642990e904243d
amoghmanoranjith <amoghmanoranjith@gmail.com>
2026-06-03 08:49:18 +0530

    added jackson object mapper for yaml in a config

---
+++ src/main/java/mp/org/blip/config/ObjectMapperConfig.java
@@ -0,0 +1,25 @@
+package mp.org.blip.config;
+
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper yamlObjectMapper() {
+        ObjectMapper mapper =
+                new ObjectMapper(new YAMLFactory());
+
+        mapper.configure(
+                DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+                true
+        );
+
+        return mapper;
+    }
+}
