f4ed7df193bcaf8152341f6143045eab70820477
amoghmanoranjith <amoghmanoranjith@gmail.com>
2026-06-13 17:14:46 +0530

    Regexp was not accepting number range step for the first field

--- src/main/java/mp/org/blip/definition/trigger/CronConfigDefinition.java
+++ src/main/java/mp/org/blip/definition/trigger/CronConfigDefinition.java
@@ -13,6 +13,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CronConfigDefinition {
     @NotBlank(message = "{cron.schedule.required}")
-    @Pattern(message = "{cron.schedule.invalid}",regexp = "^(\\*|\\?|\\b([0-5]?\\d)(-[0-5]?\\d)?(/[0-5]?\\d)?\\b)( (\\*|\\?|\\b([0-5]?\\d)(-[0-5]?\\d)?(/[0-5]?\\d)?\\b)){2}( (\\*|\\?|\\b([01]?\\d|2[0-3])(-([01]?\\d|2[0-3]))?(/([01]?\\d|2[0-3]))?\\b)){3}( (\\*|\\?|\\b([1-9]|[12]\\d|3[01])(-([1-9]|[12]\\d|3[01]))?(/([1-9]|[12]\\d|3[01]))?(L|W)?\\b)){4}( (\\*|\\?|\\b([1-9]|1[0-2])(-([1-9]|1[0-2]))?(/([1-9]|1[0-2]))?\\b|\\b(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)\\b)){5}( (\\*|\\?|\\b([1-7])(-([1-7]))?(/([1-7]))?(L|#\\d)?\\b|\\b(SUN|MON|TUE|WED|THU|FRI|SAT)\\b))$")
+    @Pattern(message = "{cron.schedule.invalid}", regexp = "(\\*(/\\d+)?|\\?|\\b([0-5]?\\d)(-[0-5]?\\d)?(/[0-5]?\\d)?\\b)")
     private String schedule;
 }
